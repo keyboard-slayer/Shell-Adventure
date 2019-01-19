770f04b6ebb27f617909ad183ee8b32be9a0cf1f
0v3rl0w <dalcjor@libmail.eu>
2019-01-19 13:37:23 +0100

    Interpreter updated Wait the parser for 2 new instructions

--- game/lang/Interpreter.py
+++ game/lang/Interpreter.py
@@ -12,7 +12,13 @@ class Interpreter:
         self.quest = quest
         self.lexer = Lexer()
         self.parser = Parser()
-        self.buffer = {'FILE': {}, 'DIR': {}, 'TIME': {}}
+        self.buffer = {
+            'FILE': ({}, os.path.isfile), 
+            'DIR': ({},  os.path.isdir),
+            'TIME': ({}, lambda time: datetime.datetime.now() > time),
+            'DELFILE': ({}, lambda fichier: not os.path.isfile(fichier)),
+            'DELDIR': ({}, lambda dossier: not os.path.isdir(dossier)) 
+        }
 
     def parse(self, command):
         lex = self.lexer.tokenize(command)
@@ -29,13 +35,11 @@ class Interpreter:
                 if code[0] == "WAIT":
                     # print(code[1])
                     if code[1][-1] == 's':
-                        self.buffer['TIME'][datetime.datetime.now() + datetime.timedelta(0, int(code[1][:-1]))] = code[2]
+                        self.buffer['TIME'][0][datetime.datetime.now() + datetime.timedelta(0, int(code[1][:-1]))] = code[2]
                         print(self.buffer['TIME'])
-                    if code[1] == 'DIR':
-                        self.buffer['DIR'][code[2]] = code[3]
                     
-                    if code[1] == "FILE":
-                        self.buffer['FILE'][code[2]] = code[3]
+                    if code[1] in self.buffer.keys():
+                        self.buffer[code[1]][0][code[2]] = code[3]
 
             return 0
         except TypeError:
@@ -43,24 +47,15 @@ class Interpreter:
 
 
     def mainloop(self):
-        if self.buffer['TIME']:
-            time = list(self.buffer['TIME'].keys())[0]
-            if datetime.datetime.now() > time:
-                self.evaluate(self.buffer['TIME'][time])
-                del self.buffer['TIME'][time]
-        
-        if self.buffer['FILE']:
-            fichier = list(self.buffer['FILE'].keys())[0]
-            if os.path.isfile(fichier):
-                self.evaluate(self.buffer['FILE'][fichier])
-                del self.buffer['FILE'][fichier]
-        
-        if self.buffer['DIR']:
-            dossier = list(self.buffer['DIR'].keys())[0]
-            if os.path.isdir(dossier):
-                self.evaluate(self.buffer['DIR'][dossier])
-                del self.buffer['DIR'][dossier]
-
+        print(self.buffer)
+        for wait in self.buffer.keys():
+            loop = 0
+            check = self.buffer[wait][1]
+            while len(self.buffer[wait][0]) > loop:
+                if check(list(self.buffer[wait][0].keys())[loop]):
+                    self.evaluate(self.buffer[wait][0][list(self.buffer[wait][0].keys())[loop]])
+                    del self.buffer[wait][0][list(self.buffer[wait][0].keys())[loop]]
+                loop += 1
 
     def execute(self, file):
         with open(f"./game/script/{file}", 'r') as script:
